a4e9df2287ffab2a8bee6b5577f95c642e0fdc2b
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2017-07-22 18:57:34 -0500

    Minor fix

--- library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/ui/layouts/SplitButtonsLayout.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/ui/layouts/SplitButtonsLayout.kt
@@ -21,11 +21,9 @@ import android.support.annotation.IntRange
 import android.support.v7.widget.AppCompatButton
 import android.text.TextUtils
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import jahirfiquitiva.libs.kauextensions.R
-import jahirfiquitiva.libs.kauextensions.extensions.inflateView
 
 /**
  * Originally created by Aidan Follestad
@@ -38,20 +36,58 @@ open class SplitButtonsLayout:LinearLayout {
             weightSum = value.toFloat()
         }
 
+    var itemsPaddingLeft = 0
+    var itemsPaddingRight = 0
+    var itemsPaddingTop = 0
+    var itemsPaddingBottom = 0
+    var itemsPadding = 0
+    var firstItemPaddingLeft = 0
+    var firstItemPaddingRight = 0
+    var firstItemPaddingTop = 0
+    var firstItemPaddingBottom = 0
+    var firstItemPadding = 0
+
     constructor(context:Context):super(context) {
-        init()
+        init(context, null)
     }
 
     constructor(context:Context, attributeSet:AttributeSet):super(context, attributeSet) {
-        init()
+        init(context, attributeSet)
     }
 
     constructor(context:Context, attributeSet:AttributeSet, defStyleAttr:Int)
             :super(context, attributeSet, defStyleAttr) {
-        init()
+        init(context, attributeSet)
     }
 
-    private fun init() {
+    private fun init(context:Context, attributeSet:AttributeSet?) {
+        attributeSet?.let {
+            val a = context.obtainStyledAttributes(it, R.styleable.SplitButtonsLayout)
+            try {
+                itemsPaddingLeft = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_itemsPaddingLeft, 0)
+                itemsPaddingRight = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_itemsPaddingRight, 0)
+                itemsPaddingTop = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_itemsPaddingTop, 0)
+                itemsPaddingBottom = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_itemsPaddingBottom, 0)
+                itemsPadding = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_itemsPadding, 0)
+                firstItemPaddingLeft = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_firstItemPaddingLeft, 0)
+                firstItemPaddingRight = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_firstItemPaddingRight, 0)
+                firstItemPaddingTop = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_firstItemPaddingTop, 0)
+                firstItemPaddingBottom = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_firstItemPaddingBottom, 0)
+                firstItemPadding = a.getDimensionPixelSize(
+                        R.styleable.SplitButtonsLayout_firstItemPadding, 0)
+            } finally {
+                a.recycle()
+            }
+        }
         orientation = HORIZONTAL
         if (isInEditMode) {
             buttonCount = 2
@@ -64,23 +100,33 @@ open class SplitButtonsLayout:LinearLayout {
 
     fun addButton(text:String, link:String) {
         if (hasAllButtons()) throw IllegalStateException("$buttonCount buttons already added")
-        val nButton:AppCompatButton = context.inflateView(R.layout.item_split_button,
-                                                          this) as AppCompatButton
+        val button:AppCompatButton = AppCompatButton(context, null, R.style.SplitButton)
         val lParams:LayoutParams = LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1F)
         try {
-            val thisParams = layoutParams as LinearLayout.LayoutParams
-            if (thisParams.gravity == Gravity.START && childCount < 1) {
-                nButton.setPadding(0, nButton.paddingTop, nButton.paddingRight,
-                                   nButton.paddingLeft)
+            if (childCount < 1) {
+                if (firstItemPadding != 0) {
+                    button.setPadding(firstItemPadding, firstItemPadding, firstItemPadding,
+                                      firstItemPadding)
+                } else {
+                    button.setPadding(firstItemPaddingLeft, firstItemPaddingTop,
+                                      firstItemPaddingRight, firstItemPaddingBottom)
+                }
+            } else {
+                if (itemsPadding != 0) {
+                    button.setPadding(itemsPadding, itemsPadding, itemsPadding, itemsPadding)
+                } else {
+                    button.setPadding(itemsPaddingLeft, itemsPaddingTop, itemsPaddingRight,
+                                      itemsPaddingBottom)
+                }
             }
         } catch (e:Exception) {
             e.printStackTrace()
         }
-        nButton.maxLines = 1
-        nButton.ellipsize = TextUtils.TruncateAt.END
-        nButton.text = text
-        nButton.tag = link
-        addView(nButton, lParams)
+        button.maxLines = 1
+        button.ellipsize = TextUtils.TruncateAt.END
+        button.text = text
+        button.tag = link
+        addView(button, lParams)
     }
 
     fun hasAllButtons():Boolean = childCount == buttonCount
