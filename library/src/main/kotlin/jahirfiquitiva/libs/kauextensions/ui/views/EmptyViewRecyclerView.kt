992963a6b821a9c601597596e255e94498f9b5be
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2017-07-25 12:21:44 -0500

    Attempt to fix EVRV

--- library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/ui/views/EmptyViewRecyclerView.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/ui/views/EmptyViewRecyclerView.kt
@@ -23,13 +23,27 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.TextView
 import ca.allanwang.kau.utils.gone
+import ca.allanwang.kau.utils.visible
 import ca.allanwang.kau.utils.visibleIf
+import jahirfiquitiva.libs.kauextensions.extensions.secondaryTextColor
 
 open class EmptyViewRecyclerView:RecyclerView {
     var loadingView:View? = null
+        set(value) {
+            field = value
+            field?.gone()
+        }
     var emptyView:View? = null
+        set(value) {
+            field = value
+            field?.gone()
+        }
     var textView:TextView? = null
-    
+        set(value) {
+            field = value
+            field?.gone()
+        }
+
     var loadingTextRes:Int = -1
     var emptyTextRes:Int = -1
 
@@ -45,61 +59,72 @@ open class EmptyViewRecyclerView:RecyclerView {
     constructor(context:Context, attributeSet:AttributeSet, defStyleAttr:Int)
             :super(context, attributeSet, defStyleAttr)
 
-    private fun updateState() {
-        if (adapter != null) {
-            val items = adapter.itemCount
-            if (items > 0) {
-                state = State.NORMAL
-            } else {
-                state = State.EMPTY
-            }
-        } else {
-            state = State.LOADING
-        }
-    }
-
     @SuppressLint("SwitchIntDef")
     private fun updateStateViews() {
-        if (state == State.LOADING) {
-            if (loadingTextRes != -1) textView?.text = context.getString(loadingTextRes)
-        } else if (state == State.EMPTY) {
-            if (emptyTextRes != -1) textView?.text = context.getString(emptyTextRes)
+        when (state) {
+            State.LOADING -> {
+                loadingView?.visible()
+                emptyView?.gone()
+                if (loadingTextRes != -1)
+                    textView?.text = context.getString(loadingTextRes)
+                gone()
+            }
+            State.NORMAL -> {
+                if (adapter != null) {
+                    val items = adapter.itemCount
+                    if (items > 0) {
+                        loadingView?.gone()
+                        emptyView?.gone()
+                        visible()
+                        state = State.NORMAL
+                    } else {
+                        state = State.EMPTY
+                    }
+                } else {
+                    state = State.LOADING
+                }
+            }
+            State.EMPTY -> {
+                loadingView?.gone()
+                emptyView?.visible()
+                if (emptyTextRes != -1)
+                    textView?.text = context.getString(emptyTextRes)
+                gone()
+            }
         }
+        textView?.setTextColor(context.secondaryTextColor)
         textView?.visibleIf(state != State.NORMAL)
-        loadingView?.visibleIf(state == State.LOADING)
-        emptyView?.visibleIf(state == State.EMPTY)
-        visibleIf(state == State.NORMAL)
     }
 
     internal val observer:RecyclerView.AdapterDataObserver = object:RecyclerView.AdapterDataObserver() {
         override fun onChanged() {
             super.onChanged()
-            updateState()
+            updateStateViews()
         }
 
         override fun onItemRangeChanged(positionStart:Int, itemCount:Int, payload:Any?) {
             super.onItemRangeChanged(positionStart, itemCount, payload)
-            updateState()
+            updateStateViews()
         }
 
         override fun onItemRangeChanged(positionStart:Int, itemCount:Int) {
             super.onItemRangeChanged(positionStart, itemCount)
-            updateState()
+            updateStateViews()
         }
 
         override fun onItemRangeInserted(positionStart:Int, itemCount:Int) {
             super.onItemRangeInserted(positionStart, itemCount)
-            updateState()
+            updateStateViews()
         }
 
         override fun onItemRangeRemoved(positionStart:Int, itemCount:Int) {
             super.onItemRangeRemoved(positionStart, itemCount)
-            updateState()
+            updateStateViews()
         }
 
         override fun onItemRangeMoved(fromPosition:Int, toPosition:Int, itemCount:Int) {
             super.onItemRangeMoved(fromPosition, toPosition, itemCount)
-            updateState()
+            updateStateViews()
         }
     }
 
@@ -108,7 +133,7 @@ open class EmptyViewRecyclerView:RecyclerView {
         oldAdapter?.unregisterAdapterDataObserver(observer)
         super.setAdapter(adapter)
         adapter?.registerAdapterDataObserver(observer)
-        updateState()
+        updateStateViews()
     }
 
     enum class State {
