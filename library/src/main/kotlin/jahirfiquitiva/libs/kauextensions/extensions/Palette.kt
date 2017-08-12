d23f7d0645a77b41cba3ee66f9f1f3d7e6453375
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2017-08-12 12:17:47 -0500

    Minor fix

--- library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/extensions/Palette.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/kauextensions/extensions/Palette.kt
@@ -25,18 +25,18 @@ fun Palette.isColorLight() = bestSwatch?.rgb?.isColorLight() ?: false
 
 val Palette.bestSwatch:Palette.Swatch?
     get() {
+        dominantSwatch?.let { return it }
         vibrantSwatch?.let { return it }
+        mutedSwatch?.let { return it }
         lightVibrantSwatch?.let { return it }
         darkVibrantSwatch?.let { return it }
-        mutedSwatch?.let { return it }
         lightMutedSwatch?.let { return it }
         darkMutedSwatch?.let { return it }
-        if (swatches.isNotEmpty()) return getBestPaletteSwatch(
-                swatches)
+        if (swatches.isNotEmpty()) return getBestPaletteSwatch(swatches)
         return null
     }
 
-fun getBestPaletteSwatch(swatches:List<Palette.Swatch>):Palette.Swatch =
+private fun getBestPaletteSwatch(swatches:List<Palette.Swatch>):Palette.Swatch =
         Collections.max<Palette.Swatch>(swatches) { opt1, opt2 ->
             val a = opt1?.population ?: 0
             val b = opt2?.population ?: 0
