47a31890819e9b6fb8c311729e14dc189099feda
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-01-21 15:36:33 -0500

    Minor fix

--- core/src/main/kotlin/jahirfiquitiva/libs/kauextensions/extensions/Menu.kt
+++ core/src/main/kotlin/jahirfiquitiva/libs/kauextensions/extensions/Menu.kt
@@ -18,6 +18,7 @@ package jahirfiquitiva.libs.kauextensions.extensions
 import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.view.Menu
+import android.view.MenuItem
 
 fun Menu.changeOptionVisibility(id: Int, visible: Boolean) {
     findItem(id)?.isVisible = visible
@@ -33,4 +34,52 @@ fun Menu.setOptionIcon(id: Int, @DrawableRes iconRes: Int) {
 
 fun Menu.setOptionIcon(id: Int, iconRes: Drawable) {
     findItem(id)?.icon = iconRes
+}
+
+fun Menu.getItems(): ArrayList<MenuItem?> {
+    val items = ArrayList<MenuItem?>()
+    for (i in 0 until size()) {
+        items += getItem(i)
+    }
+    return items
+}
+
+fun Menu.showAllItems() {
+    getItems().forEach { it?.show() }
+}
+
+fun Menu.hideAllItems() {
+    getItems().forEach { it?.hide() }
+}
+
+fun Menu.showAllExcept(toKeepHidden: ArrayList<Int>, hideIfShown: Boolean = true) {
+    getItems().forEach {
+        it?.let {
+            if (!toKeepHidden.contains(it.itemId)) {
+                it.show()
+            } else {
+                if (hideIfShown) else it.hide()
+            }
+        }
+    }
+}
+
+fun Menu.hideAllExcept(toKeepShown: ArrayList<Int>, showIfHidden: Boolean = true) {
+    getItems().forEach {
+        it?.let {
+            if (!toKeepShown.contains(it.itemId)) {
+                it.hide()
+            } else {
+                if (showIfHidden) else it.show()
+            }
+        }
+    }
+}
+
+fun MenuItem.show() {
+    isVisible = true
+}
+
+fun MenuItem.hide() {
+    isVisible = false
 }
