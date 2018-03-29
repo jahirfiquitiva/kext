d568214ca0d09ab4a1c0d082d0649238b112054b
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-03-28 22:16:28 -0500

    Add view model exceptions

---
+++ archhelpers/src/main/kotlin/jahirfiquitiva/libs/archhelpers/extensions/ArchExtensions.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018. Jahir Fiquitiva
+ *
+ * Licensed under the CreativeCommons Attribution-ShareAlike
+ * 4.0 International License. You may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *    http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jahirfiquitiva.libs.archhelpers.extensions
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+
+/**
+ * Extensions based on:
+ * https://antonioleiva.com/architecture-components-kotlin/
+ */
+
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(): T =
+        ViewModelProviders.of(this)[T::class.java]
+
+inline fun <reified T : ViewModel> Fragment.getViewModel(): T =
+        ViewModelProviders.of(this)[T::class.java]
